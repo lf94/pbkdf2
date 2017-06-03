d0b1b1e8dafe0a9bcd47e4b2097a696c6c5ad2e6
Lee <ircsurfer33@gmail.com>
2017-06-03 14:43:35 -0400

    Make my head hurt less

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 
-/* istanbul ignore next */
+exports.pbkdf2 = require('./lib/async')
 
-if (window) {
-  var crypto = require('./browser');
-} else {
-  var crypto = require('crypto')
-}
-
-exports.pbkdf2Sync = crypto.pbkdf2Sync
-exports.pbkdf2 = crypto.pbkdf2
+exports.pbkdf2Sync = require('./lib/sync')
