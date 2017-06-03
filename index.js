d4d0ad75b546c0476c06e024fa1e4eaa90fb928d
Lee <ircsurfer33@gmail.com>
2017-06-03 12:07:00 -0400

    Use browser.js if not on node

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var crypto = require('crypto')
 /* istanbul ignore next */
-if (crypto.pbkdf2Sync.toString().indexOf('keylen, digest') === -1) {
-  throw new Error('Unsupported crypto version')
+if (!crypto.pbkdf2Sync) {
+  var crypto = require('./browser');
 }
 
 exports.pbkdf2Sync = crypto.pbkdf2Sync
